e3ef30b0dc6c24a1fe94112897da4d707cee2aa0
sarthak <sarthak.ahuja0007@gmail.com>
2021-07-21 04:23:29 +0530

    CRUD operations MongoDB node

--- db/index.js
+++ db/index.js
@@ -1,13 +1,31 @@
 const MongoClient = require("mongodb").MongoClient;
-const url = 'mongodb://locahost:27017'
+const operations = require('./operations');
+const url = 'mongodb://localhost:27017'
 
 const dbname = 'conFusion';
 
 MongoClient.connect(url)
-.then((client)=>{
-    const db=client.db(dbname);
-    const collection=db.collection('dishes');
-})
-.catch(err=>{
-    console.log(err);
-})
+    .then((client) => {
+        const db = client.db(dbname);
+        operations.insertDocument(db, { name: "Test", description: "Testsssss" }, "dishes")
+            .then((result) => {
+                console.log(result);
+                operations.findDocuments(db, "dishes")
+                    .then(docs => {
+                        console.log(docs);
+                        operations.removeDocument(db, { name: "Test" }, "dishes")
+                            .then(result => {
+                                console.log(result);
+                                operations.updateDocument(db, { name: "Test" }, { description: "Updated Test" },"dishes")
+                            })
+                            .catch(err => console.log(err))
+                    })
+                    .catch(err => console.log(err))
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+    .catch(err => {
+        console.log(err);
+    })
--- db/operations.js
+++ db/operations.js
@@ -1,38 +1,52 @@
-exports.insertDocument =
-
-    // (db, document, collection, callback) => {
-    //     const coll = db.collection(collection);
-    //     coll.insert(document, (err, result) => {
-    //         assert.equal(err, null);
-    //         console.log("Inserted " + result.result.n +
-    //             " documents into the collection " + collection);
-    //         callback(result);
-    //     });
-    // };
-
-
-    exports.findDocuments = (db, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
+    return new Promise((resolve, reject) => {
         const coll = db.collection(collection);
-        coll.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
-            callback(docs);
-        });
-    };
+        coll.insertOne(document)
+            .then((result) => {
+                resolve(result)
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+}
 
-exports.removeDocument = (db, document, collection, callback) => {
-    const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result);
-    });
+exports.findDocuments = (db, collection) => {
+    return new Promise((resolve, reject) => {
+        const coll = db.collection(collection);
+        coll.find({}).toArray()
+            .then((docs) => {
+                resolve(docs)
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
-    const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result);
-    });
+exports.removeDocument = (db, document, collection) => {
+    return new Promise((resolve, reject) => {
+        const coll = db.collection(collection);
+        coll.deleteOne(document)
+            .then((result) => {
+                resolve(result)
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+    return new Promise((resolve, reject) => {
+        const coll = db.collection(collection);
+        coll.updateOne(document,{$set:update})
+            .then((result) => {
+                resolve(result)
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
 };
