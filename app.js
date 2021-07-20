8b9298504691749bd04c919f0d86aa037121563b
sarthak <sarthak.ahuja0007@gmail.com>
2021-07-20 17:58:58 +0530

    promo router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
+app.use('/promos', promoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
