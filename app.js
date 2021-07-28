c8448685ce589fae7dff30d2de21f535953d170b
sarthak <sarthak.ahuja0007@gmail.com>
2021-07-28 13:24:51 +0530

    https and openSSL certificate, private key

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ var authenticate = require('./authenticate');
 const passport = require('passport');
 
 var app = express();
-
+//Transferring http to https
+app.all('*', (req, res, next) => {
+	if (req.secure) {
+		return next();
+	}
+	else {
+		res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+	}
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
