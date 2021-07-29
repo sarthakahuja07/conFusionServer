e3c48e31765c22ca3690909b7f18adaa53cd5b4e
sarthak <sarthak.ahuja0007@gmail.com>
2021-07-29 19:02:59 +0530

    passport facebook init

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': 'sarthak07',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '583774599459571',
+        clientSecret: '1b1e13609f20ede29319ac5ee9b6189b'
+    }
 }
